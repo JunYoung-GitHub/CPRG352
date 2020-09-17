08b7229e2c9117fca9633f33b9f1f27756b3a5c2
710429 <710429@SAIT224192IT.ACDM.DS.SAIT.CA>
2020-09-17 12:54:24 -0600

    New Project Creation Created a new Java Application for HelloWorld.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Author: Jun Young Won
+ * Version: Sept 17 2020
  */
 package helloworld;
-
-/**
- *
- * @author 710429
- */
 public class HelloWorld {
 
     /**
